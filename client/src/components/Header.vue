661363fab67fcf6a5ca5919c1ff97765db9deeaf
Ivan <ipzent@gmail.com>
2018-02-21 09:35:17 -0800

    latest

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,6 +1,18 @@
 <template>
   <v-toolbar app color="primary">
     <v-toolbar-title class="mr-4" dark>PowerApp</v-toolbar-title>
+    <v-toolbar-items>
+     <v-btn flat dark>
+       Browse
+     </v-btn:flat:dark>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn flat dark>
+        Sign Up
+      </v-btn>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
 
